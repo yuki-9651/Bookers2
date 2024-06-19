651a9ee2611732774f171223e03f401348a2bfb8
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-06-19 04:47:36 +0000

    [ADD]bookのindexページの右側の作成(Books)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,20 +5,23 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     
-    book.save
-   
-    redirect_to '/top'
+    if @book.save
+      redirect_to books_path, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
   end
 
   def index
-    @book = Book.new(book_params)
+    @book = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
